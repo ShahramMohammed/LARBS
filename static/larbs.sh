3d4eb6c83f304b98da939213021763edcd694c2c
Shahram <80352285+ShahramMohammed@users.noreply.github.com>
2024-11-08 15:52:01 +0300

    Change dotfiles repo to my own fork

--- static/larbs.sh
+++ static/larbs.sh
@@ -6,7 +6,7 @@
 
 ### OPTIONS AND VARIABLES ###
 
-dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
+dotfilesrepo="git@github.com:ShahramMohammed/dotfiles.git"
 progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/static/progs.csv"
 aurhelper="yay"
 repobranch="master"
